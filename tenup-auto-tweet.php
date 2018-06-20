fe5949ca431be457eeb65baf2d67b60c01f3e66e
Scott Lee <scott@10up.com>
2018-06-20 17:02:13 -0500

    - Main plugin file. - Settings - Composer

---
+++ tenup-auto-tweet.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Plugin Name: 10up Auto Tweet
+ * Description: Adds the ability to automatically publish a status update to Twitter.
+ * Version:     0.1.0
+ * Author:      10up
+ * Author URI:  https://10up.com
+ * License:     GPLv2 or later
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+define( 'TUAT_VERSION', '0.1.0' );
+define( 'TUAT_URL', plugin_dir_url( __FILE__ ) );
+define( 'TUAT_PATH', plugin_dir_path( __FILE__ ) );
+define( 'TUAT_INC', TUAT_PATH . 'includes/' );
+
+/**
+ * Composer check
+ */
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+} else {
+	add_action( 'admin_notices', function() {
+		printf(
+			'<div class="error notice"><p>%s</p></div>',
+			__( 'The 10up Auto Tweet plugin requires composer to be installed. Please run `composer install` to get started.', 'tenup_auto_tweet' )
+		);
+	} );
+}
+
+// Include the main functionality
+require_once( 'includes/core.php' );
+//require_once( 'includes/utils.php' );
+
+/**
+ * Play nice with others.
+ */
+do_action( 'tenup_auto_tweet_loaded' );
