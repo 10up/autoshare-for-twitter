bc79c18cedcf04cd67a4e7a0c49fbb21f565eaac
Scott Lee <scott@10up.com>
2018-06-20 17:01:54 -0500

    Remove everything.

---
+++ includes/core.php
@@ -0,0 +1,26 @@
+<?php
+namespace TenUp\Auto_Tweet\Core;
+
+/**
+ * The main setup action.
+ */
+function setup() {
+
+	// Includes and requires
+	require_once 'admin/settings.php';
+	//require_once 'admin/post-meta.php';
+	//require_once 'admin/post-transition.php';
+	//require_once 'class-publish_tweet.php';
+
+	/**
+	 * Allow others to hook into the core setup action
+	 */
+	do_action( 'tenup_auto_tweet_setup' );
+}
+
+/**
+ * Fire up the module.
+ *
+ * @uses auto_tweet_loaded
+ */
+add_action( 'tenup_auto_tweet_loaded', __NAMESPACE__ . '\setup' );
--- script.js
+++
@@ -1,3 +0,0 @@
-/**
- * Tenup Auto Tweet
- */
