fd7d152304a0b8809189d3b2116901ab85a8a232
John Watkins <john.watkins@get10up.com>
2019-10-30 23:59:00 -0500

    Fix main plugin filename

---
+++ autoshare-for-twitter.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Plugin Name:       Auto Share for Twitter
+ * Description:       Automatically tweets the post title or custom message and a link to the post.
+ * Disclaimer:        TWITTER, TWEET, RETWEET and the Twitter logo are trademarks of Twitter, Inc. or its affiliates.
+ * Version:           1.0.0
+ * Requires at least: 4.7
+ * Requires PHP:      7.0
+ * Author:            10up
+ * Author URI:        https://10up.com
+ * License:           GPL-2.0-or-later
+ * License URI:       https://spdx.org/licenses/GPL-2.0-or-later.html
+ *
+ * @package TenUp\Autoshare
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+define( 'AUTOSHARE', __FILE__ );
+define( 'TUAT_VERSION', '1.0.0' );
+define( 'TUAT_URL', plugin_dir_url( __FILE__ ) );
+define( 'TUAT_PATH', plugin_dir_path( __FILE__ ) );
+define( 'TUAT_INC', TUAT_PATH . 'includes/' );
+
+/**
+ * Composer check.
+ */
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+	require_once __DIR__ . '/vendor/autoload.php';
+}
+
+
+// Include the main functionality.
+require_once plugin_dir_path( __FILE__ ) . 'includes/core.php';
+require_once plugin_dir_path( __FILE__ ) . 'includes/utils.php';
+
+/**
+ * Play nice with others.
+ */
+do_action( 'autoshare_loaded' );
