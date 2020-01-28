daf96acddedf640176d97aaebf87d2e1595e4ca6
John Watkins <john.watkins@get10up.com>
2020-01-28 17:30:44 -0600

    Remove extra autoshare_for_twitter_tweet filter

--- includes/class-publish-tweet.php
+++ includes/class-publish-tweet.php
@@ -126,7 +126,6 @@ class Publish_Tweet {
 		 * @param WP_Post    The post associated with the tweet.
 		 */
 		$response    = apply_filters( 'autoshare_for_twitter_pre_status_update', null, $update_data, $post );
-		$update_data = apply_filters( 'autoshare_for_twitter_tweet', $update_data, $post );
 
 		if ( ! is_null( $response ) ) {
 			return $response;
