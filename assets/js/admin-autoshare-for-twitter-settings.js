46e0cafcb3a2b9d54bc49fdaad6f2003aea84618
Tung Du <dinhtungdu@gmail.com>
2020-02-05 07:55:16 +0700

    add: show/hide post types settings

--- assets/js/admin-autoshare-for-twitter-settings.js
+++ assets/js/admin-autoshare-for-twitter-settings.js
@@ -2,16 +2,10 @@
 
 	document.addEventListener( 'DOMContentLoaded', function() {
 		var credActions = document.querySelector( '.credentials-actions' ),
-			openCredSettingsBtn,
-			closeCredSettingsBtn;
-
-		if( ! credActions ) {
-			return;
-		}
-
-		openCredSettingsBtn = credActions.querySelector( '.open' );
-		closeCredSettingsBtn = credActions.querySelector( '.close' );
-	;
+			openCredSettingsBtn = credActions.querySelector( '.open' ),
+			closeCredSettingsBtn = credActions.querySelector( '.close' ),
+			postTypesWrap = document.querySelector( '.post-types' ),
+			postTypesCheckboxes = document.getElementsByName( 'autoshare-for-twitter[enable_for]' );
 
 		openCredSettingsBtn.addEventListener( 'click', function() {
 			credActions.classList.remove( 'connected' );
@@ -21,5 +15,50 @@
 			credActions.classList.add( 'connected' );
 		} );
 
+		postTypesCheckboxes.forEach( function( item ) {
+			item.addEventListener( 'change', function( event) {
+				if ( event.target.value === 'all' )
+					return postTypesWrap.classList.add( 'hidden' );
+				return postTypesWrap.classList.remove( 'hidden' );
+			} );
+		} );
+
+
 	} );
+
+	/**
+	 * Get closest Element.
+	 *
+	 * @param {HTMLElement} el Element to get parent.
+	 * @param {string} selector CSS selector to match.
+	 */
+	function getClosest( el, selector ) {
+		if ( ! window.Element.prototype.matches ) {
+			// Polyfill from https://developer.mozilla.org/en-US/docs/Web/API/Element/matches.
+			window.Element.prototype.matches =
+				window.Element.prototype.matchesSelector ||
+				window.Element.prototype.mozMatchesSelector ||
+				window.Element.prototype.msMatchesSelector ||
+				window.Element.prototype.oMatchesSelector ||
+				window.Element.prototype.webkitMatchesSelector ||
+				function( s ) {
+					var matches = ( this.document || this.ownerDocument ).querySelectorAll( s ),
+						i = matches.length;
+
+					while ( --i >= 0 && matches.item( i ) !== this ) { }
+
+					return i > -1;
+				};
+		}
+
+		// Get the closest matching elent
+		for ( ; el && el !== document; el = el.parentNode ) {
+			if ( el.matches( selector ) ) {
+				return el;
+			}
+		}
+
+		return null;
+	}
+
 } )();
