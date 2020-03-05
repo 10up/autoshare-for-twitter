f977544f45e888948f760f387d9bff3072569b06
Tung Du <dinhtungdu@gmail.com>
2020-03-05 11:25:06 +0700

    fix: typo and cleanup

--- assets/js/admin-autoshare-for-twitter-settings.js
+++ assets/js/admin-autoshare-for-twitter-settings.js
@@ -26,39 +26,4 @@
 
 	} );
 
-	/**
-	 * Get closest Element.
-	 *
-	 * @param {HTMLElement} el Element to get parent.
-	 * @param {string} selector CSS selector to match.
-	 */
-	function getClosest( el, selector ) {
-		if ( ! window.Element.prototype.matches ) {
-			// Polyfill from https://developer.mozilla.org/en-US/docs/Web/API/Element/matches.
-			window.Element.prototype.matches =
-				window.Element.prototype.matchesSelector ||
-				window.Element.prototype.mozMatchesSelector ||
-				window.Element.prototype.msMatchesSelector ||
-				window.Element.prototype.oMatchesSelector ||
-				window.Element.prototype.webkitMatchesSelector ||
-				function( s ) {
-					var matches = ( this.document || this.ownerDocument ).querySelectorAll( s ),
-						i = matches.length;
-
-					while ( --i >= 0 && matches.item( i ) !== this ) { }
-
-					return i > -1;
-				};
-		}
-
-		// Get the closest matching elent
-		for ( ; el && el !== document; el = el.parentNode ) {
-			if ( el.matches( selector ) ) {
-				return el;
-			}
-		}
-
-		return null;
-	}
-
 } )();
