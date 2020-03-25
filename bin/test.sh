588e5460037f30d73412f59870b4d327cddf8957
John Watkins <johnwatkins0@gmail.com>
2020-03-24 22:39:13 -0500

    Remove linting for 5.6 tests

--- bin/test.sh
+++ bin/test.sh
@@ -1,28 +1,11 @@
-if [ '5.6' == $PHP_VERSION ]; then
+if [ '5.6' == $PHP_VERSION || '7.0' == $PHP_VERSION || '7.1' == $PHP_VERSION  ]; then
     php -v
-    rm composer.lock
-    mv composer.json composer.json.bak
+    rm composer.lock composer.json
     cp bin/composer-5.6.json composer.json
     composer install
     composer run lint
     composer run setup-local-tests
     composer run test
-    rm composer.lock composer.json
-    mv composer.json.bak composer.json
-    composer install
-fi;
-
-if [[ '7.0' == $PHP_VERSION || '7.1' == $PHP_VERSION ]]; then
-    php -v
-    rm composer.lock
-    mv composer.json composer.json.bak
-    cp bin/composer-7.1.json composer.json
-    composer install
-    composer run setup-local-tests
-    composer run test
-    rm composer.lock composer.json
-    mv composer.json.bak composer.json
-    composer install
 fi;
 
 if [[ '7.2' == $PHP_VERSION || '7.3' == $PHP_VERSION ]]; then
