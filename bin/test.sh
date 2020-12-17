f7f68a0fcaf92b95877a25b1a05395456368cce6
Tung Du <dinhtungdu@gmail.com>
2020-12-17 15:40:23 +0700

    fix: composer issue

--- bin/test.sh
+++ bin/test.sh
@@ -4,14 +4,14 @@ if [[ '5.6' == $PHP_VERSION || '7.0' == $PHP_VERSION || '7.1' == $PHP_VERSION  ]
     php -v
     rm composer.lock composer.json
     cp bin/composer-5.6.json composer.json
-    composer install
+    composer install --ignore-platform-reqs
     composer run setup-local-tests
     composer run test
 fi;
 
 if [[ '7.2' == $PHP_VERSION ]]; then
     php -v
-    composer install
+    composer install --ignore-platform-reqs
     composer run lint
     composer run setup-local-tests
     composer run test
