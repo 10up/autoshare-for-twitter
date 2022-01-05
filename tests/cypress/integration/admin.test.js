7b88796a6cb27ab8e0394e3d38ac867e0cd8d94d
thrijith <thrijith13@gmail.com>
2022-01-05 18:45:21 +0530

    Add option to set twitter creds
    Update permission of test core version setting
    
    Update description for edit page
    
    Remove test for actual tweet test
    
    Remove secrets passed in test action

--- tests/cypress/integration/admin.test.js
+++ tests/cypress/integration/admin.test.js
@@ -18,11 +18,22 @@ describe( 'Plugin settings page has the necessary fields', () => {
 } );
 
 describe( 'Plugin is not configured', () => {
-	it( 'Can see all the fields on the settings page', () => {
+	it( 'Can autoshare post status div', () => {
 		cy.visitAdminPage( 'post.php?post=1&action=edit' );
+		cy.wait( 10000 );
 		cy.get( 'button[aria-label="Close dialog"' ).click();
 		cy.get( 'div.autoshare-for-twitter-post-status' ).should( 'be.visible' );
 	} );
 } );
 
-// Todo: Add Twitter credentials here.
+describe( 'Configure the plugin', () => {
+	it( 'Configure the plugin secrets', () => {
+		cy.visitAdminPage( 'options-general.php?page=autoshare-for-twitter' );
+		cy.get( '.large-text:nth-child(1) .large-text' ).type( 'abcDEf' );
+		cy.get( '.large-text:nth-child(2) .large-text' ).type( 'ghIJklm' );
+		cy.get( '.large-text:nth-child(3) .large-text' ).type( 'noPQrst' );
+		cy.get( '.large-text:nth-child(4) .large-text' ).type( 'uvWXyz' );
+		cy.get( '.regular-text' ).type( 'gh_issue_help' );
+		cy.get( '#submit' ).click();
+	} );
+} );
--- tests/cypress/integration/block-editor.test.js
+++ tests/cypress/integration/block-editor.test.js
@@ -3,6 +3,7 @@ const slug = 'autoshare-for-twitter';
 describe( 'Turn off default autoshare and make sure post is not tweeted', () => {
 	it( 'Autoshare disable default', () => {
 		cy.visitAdminPage( 'options-general.php?page=autoshare-for-twitter' );
+		cy.wait( 5000 );
 		cy.get( 'input[name="autoshare-for-twitter[enable_default]"]' ).eq( 1 ).click();
 		cy.get( '#submit' ).click();
 	} );
@@ -25,26 +26,5 @@ describe( 'Turn off default autoshare and make sure post is not tweeted', () => 
 		cy.get( '.autoshare-for-twitter-post-status' ).contains( 'This post was not tweeted.' );
 	} );
 
-	it( 'Tests that new post is tweeted when box is checked', () => {
-		cy.visitAdminPage( 'post-new.php' );
-		cy.get( 'button[aria-label="Close dialog"' ).click();
-		cy.get( '#post-title-0' ).type( 'Just another random test post title' );
-
-		cy.get( '.editor-post-publish-panel__toggle', { timeout: 10000 } ).should( 'be.visible' );
-		cy.get( '.editor-post-publish-panel__toggle' ).click();
-		cy.wait( 5000 );
-
-		cy.get( 'button' ).contains( 'Autoshare:' ).click();
-		cy.get( '.autoshare-for-twitter-prepublish__checkbox-label', { timeout: 10000 } ).should( 'be.visible' );
-		cy.get( '.autoshare-for-twitter-prepublish__checkbox-label' ).click();
-		cy.wait( 5000 );
-
-		cy.get( '[aria-disabled="false"].editor-post-publish-button__button', { timeout: 10000 } ).should( 'be.visible' );
-		cy.get( '.editor-post-publish-button' ).click();
-
-		// Post-publish.
-		cy.get( '.autoshare-for-twitter-post-status' ).contains( 'This post was not tweeted.' ).should( 'not.exist' );
-		cy.get( '.autoshare-for-twitter-post-status' ).contains( 'Tweeted on' );
-	} );
 } );
 
