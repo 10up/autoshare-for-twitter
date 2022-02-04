3c8d2de654a9ff8adbdc61ceb1ad95656514bc9f
thrijith <thrijith13@gmail.com>
2022-02-04 23:26:29 +0530

    Update tests and add  block editor and classic editor cypress tests Remove wpacceptance

--- tests/cypress/integration/block-editor.test.js
+++ tests/cypress/integration/block-editor.test.js
@@ -1,5 +1,14 @@
 const slug = 'autoshare-for-twitter';
 
+describe( 'Visit Classic Editor settings page', () => {
+	it( 'Update settings to keep the default editor as classic editor', () => {
+		cy.visitAdminPage( 'options-writing.php#classic-editor-options' );
+		cy.get( '#classic-editor-block' ).click();
+		cy.get( '#classic-editor-allow' ).click();
+		cy.get( '#submit' ).click();
+	} );
+} );
+
 describe( 'Tests that new post is not tweeted when box is unchecked', () => {
 	it( 'Autoshare disable default', () => {
 		cy.visitAdminPage( 'options-general.php?page=autoshare-for-twitter' );
@@ -11,7 +20,7 @@ describe( 'Tests that new post is not tweeted when box is unchecked', () => {
 	it( 'Tests that new post is not tweeted when box is unchecked', () => {
 		cy.visitAdminPage( 'post-new.php' );
 		cy.get( 'button[aria-label="Close dialog"' ).click();
-		cy.get( '#post-title-0' ).type( 'Text' );
+		cy.get( 'h1.wp-block-post-title' ).type( 'Text' );
 
 		cy.get( '[aria-disabled="false"].editor-post-publish-panel__toggle', { timeout: 10000 } ).should( 'be.visible' );
 		cy.get( '.editor-post-publish-panel__toggle' ).click();
@@ -31,18 +40,14 @@ describe( 'Tests that new post is tweeted when box is checked', () => {
 	it( 'Tests that new post is tweeted when box is checked', () => {
 		cy.visitAdminPage( 'post-new.php' );
 		cy.get( 'button[aria-label="Close dialog"' ).click();
-		cy.get( '#post-title-0' ).type( 'Random Post Title' );
+		cy.get( 'h1.wp-block-post-title' ).type( 'Random Post Title' );
 
 		cy.get( '[aria-disabled="false"].editor-post-publish-panel__toggle', { timeout: 10000 } ).should( 'be.visible' );
 		cy.get( '.editor-post-publish-panel__toggle' ).click();
 		cy.wait( 5000 );
 
-		// cy.get('.autoshare-for-twitter-prepublish__checkbox-row').parent('components-panel__body').click();
 		cy.get('.components-panel__body:nth-child(7) .editor-post-publish-panel__link').click();
-		cy.get('.is-opened .editor-post-publish-panel__link').click();
-
-
-		// cy.get( '.autoshare-for-twitter-prepublish__checkbox-label', { timeout: 10000 } ).should( 'be.visible' );
+		cy.get( '.autoshare-for-twitter-prepublish__checkbox-label', { timeout: 10000 } ).should( 'be.visible' );
 		cy.get( '.autoshare-for-twitter-prepublish__checkbox-label' ).click();
 		cy.wait( 5000 );
 
