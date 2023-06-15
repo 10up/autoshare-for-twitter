3c0a00ef2a2efb0323e5173e9d5dc2b80fd033a9
Dharmesh Patel <dspatel44@gmail.com>
2023-06-15 20:43:29 +0530

    Different approach.

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -56,28 +56,28 @@ Cypress.Commands.add( 'enableCheckbox', ( checkboxSelector, defaultBehavior, che
 	// Check/Uncheck enable checkbox for auto-share.
 	cy.get(checkboxSelector).should('exist');
 	if (true === defaultBehavior) {
-		cy.get(checkboxSelector).should('be.checked');
+		cy.get(checkboxSelector).first().should('be.checked');
 	} else {
-		cy.get(checkboxSelector).should('not.be.checked');
+		cy.get(checkboxSelector).first().should('not.be.checked');
 	}
-	const alias = `enableCheckbox${new Date().getTime()}${Math.floor(Math.random() * 999)}`;
-	cy.intercept('**/autoshare/v1/post-autoshare-for-twitter-meta/*').as(alias);
+
+	cy.intercept('**/autoshare/v1/post-autoshare-for-twitter-meta/*').as('enableCheckbox');
 	if (true === check) {
-		cy.get(checkboxSelector).check({force: true});
+		cy.get(checkboxSelector).first().check({force: true});
 		if(defaultBehavior !== check){
-			cy.wait(`@${alias}`).then(response => {
+			cy.wait('@enableCheckbox').then(response => {
 				expect(response.response?.body?.enabled).to.equal(check);
 			});
 		}
-		cy.get(checkboxSelector).should('be.checked');
+		cy.get(checkboxSelector).first().should('be.checked');
 	} else {
-		cy.get(checkboxSelector).uncheck({force: true});
+		cy.get(checkboxSelector).first().uncheck({force: true});
 		if(defaultBehavior !== check){
-			cy.wait(`@${alias}`).then(response => {
+			cy.wait('@enableCheckbox').then(response => {
 				expect(response.response?.body?.enabled).to.equal(check);
 			});
 		}
-		cy.get(checkboxSelector).should('not.be.checked');
+		cy.get(checkboxSelector).first().should('not.be.checked');
 	}
 });
 
