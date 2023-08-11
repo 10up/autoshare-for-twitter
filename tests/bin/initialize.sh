6754b541ad8402f0be7e91c20ae2c86f982f168a
Dharmesh Patel <dspatel44@gmail.com>
2023-08-11 15:40:32 +0530

    Fix WP 6.3 issue.

--- tests/bin/initialize.sh
+++ tests/bin/initialize.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 npm run env run tests-wordpress "chmod -c ugo+w /var/www/html"
 npm run env run tests-cli "wp rewrite structure '/%postname%/' --hard"
-npm run env run tests-cli "wp user meta update 1 enable_custom_fields 1"
