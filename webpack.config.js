a031ae1d341717527b5228b9c91c51885b2ee937
Dharmesh Patel <dspatel44@gmail.com>
2023-09-08 16:41:10 +0530

    Updated webpack config.

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,27 @@
-const path = require( 'path' );
+const defaultConfig = require('@wordpress/scripts/config/webpack.config');
+const path = require('path');
 
 module.exports = {
+	...defaultConfig,
+	output: {
+		...defaultConfig.output,
+		path: path.resolve(__dirname, 'dist'),
+	},
 	entry: {
 		'autoshare-for-twitter': './src/js/index.js',
-		'api-fetch': './src/js/externals/api-fetch',
+		'api-fetch': './src/js/externals/api-fetch.js'
 	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /(node_modules)/,
-				use: {
-					loader: 'babel-loader',
-				},
-			},
-		],
-	},
-	output: {
-		filename: '[name].js',
-		path: path.resolve( './dist' ),
+	externals: {
+		'admin-autoshare-for-twitter': 'adminAutoshareForTwitter',
+		'@wordpress/api-fetch': 'wp.apiFetch',
+		'@wordpress/components': 'wp.components',
+		'@wordpress/compose': 'wp.compose',
+		'@wordpress/data': 'wp.data',
+		'@wordpress/edit-post': 'wp.editPost',
+		'@wordpress/element': 'wp.element',
+		'@wordpress/i18n': 'wp.i18n',
+		'@wordpress/plugins': 'wp.plugins',
+		lodash: 'lodash',
+		react: 'React',
 	},
 };
