e86c6480079a6f54bd9cc94dd43454238f2b27b0
Jon Christensen <jon.christensen@10up.com>
2024-06-28 09:43:41 -0500

    Gutenberg editor support

--- src/js/AutoshareForTwitterPostStatusInfo.js
+++ src/js/AutoshareForTwitterPostStatusInfo.js
@@ -1,6 +1,6 @@
 import { __ } from '@wordpress/i18n';
 import { compose } from '@wordpress/compose';
-import { useState } from '@wordpress/element';
+import { useState, useEffect } from '@wordpress/element';
 import { withSelect, useSelect, select } from '@wordpress/data';
 import { Button, ToggleControl, Icon } from '@wordpress/components';
 import { TweetTextField } from './components/TweetTextField';
@@ -29,6 +29,73 @@ export function AutoshareForTwitterPostStatusInfo() {
 	} );
 
 	const [ statusMessages, setStatusMessages ] = useState( messages );
+
+	// Syndication Links plugin support.
+	// This is all so that the Syndication Links plugin can show updates in real time with our status messages.
+	// In addition, this fixes an issue where a user might actually remove a syndicated link unintentionally when clicking "update" on the post.
+	useEffect( () => {
+		// Bail if there are no status messages.
+		if ( statusMessages && ! statusMessages.message.length ) {
+			return;
+		}
+
+		// Bail if the reTweet is false. Prevents this from happening on load.
+		if ( ! reTweet ) {
+			return;
+		}
+
+		// Get the Syndication URL inputs.
+		const syndicationUrlInputs = Array.from(
+			document.querySelectorAll( 'input[name="syndication_urls[]"]' )
+		);
+
+		// Bail if there are no Syndication URL inputs.
+		if ( ! syndicationUrlInputs.length ) {
+			return;
+		}
+
+		// Get the URLs from the status messages.
+		// We'll use these to compare and populate the Syndication URL inputs.
+		const statusMessagesUrls = statusMessages.message.map( ( message ) => {
+			return message.url;
+		} );
+
+		// Get the existing URLs from the Syndication URL inputs.
+		const syndicationUrlInputsUrls = syndicationUrlInputs.map(
+			( input ) => {
+				return input.value;
+			}
+		);
+
+		// Get the Syndication URL list.
+		const syndicationUrlList = document.querySelector(
+			'.syndication_url_list ul'
+		);
+
+		statusMessagesUrls.forEach( ( url ) => {
+			// If the URL is already in the Syndication URL inputs, bail.
+			if ( syndicationUrlInputsUrls.includes( url ) ) {
+				return;
+			}
+
+			// Create the Syndication URL input list item.
+			const syndicationUrlInputListItem = document.createElement( 'li' );
+
+			// Create the Syndication URL input.
+			const syndicationUrlInput = document.createElement( 'input' );
+			syndicationUrlInput.classList.add( 'widefat' );
+			syndicationUrlInput.type = 'text';
+			syndicationUrlInput.name = 'syndication_urls[]';
+			syndicationUrlInput.value = url;
+
+			// Append the Syndication URL input to the Syndication URL list.
+			syndicationUrlInputListItem.appendChild( syndicationUrlInput );
+			syndicationUrlList.appendChild( syndicationUrlInputListItem );
+
+			// Add the URL to the Syndication URL inputs URLs so we don't repeat ourselves.
+			syndicationUrlInputsUrls.push( url );
+		} );
+	}, [ statusMessages, reTweet ] );
 
 	useSaveTwitterData();
 
