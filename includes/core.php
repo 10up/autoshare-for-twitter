13b569e997bb722589136036d6f6e1509773430c
Elvis Morales <elvis.morales@10up.com>
2025-01-06 00:34:27 -0800

    Initialize the Syndication Links integration after plugins loaded and with late priority. Syndication Links plugin initialization is at 11. Otherwise, class_exists( 'Syn_Meta' ) will evaluate to false since it doesn't still exists in context.

--- includes/core.php
+++ includes/core.php
@@ -31,10 +31,16 @@ function setup() {
 	require_once plugin_dir_path( AUTOSHARE_FOR_TWITTER ) . 'includes/class-twitter-accounts.php';
 
 	// Initialize the Syndication Links integration if plugin is active.
-	if ( class_exists( 'Syn_Meta' ) ) {
-		require_once plugin_dir_path( AUTOSHARE_FOR_TWITTER ) . 'includes/class-syndication-links.php';
-		\TenUp\AutoshareForTwitter\Core\Syndication\Syndication_Links::init();
-	}
+	add_action(
+		'plugins_loaded',
+		function() {
+			if ( class_exists( 'Syn_Meta' ) ) {
+				require_once plugin_dir_path( AUTOSHARE_FOR_TWITTER ) . 'includes/class-syndication-links.php';
+				\TenUp\AutoshareForTwitter\Core\Syndication\Syndication_Links::init();
+			}
+		},
+		99
+	);
 
 	\TenUp\AutoshareForTwitter\Admin\Assets\add_hook_callbacks();
 	\TenUp\AutoshareForTwitter\REST\add_hook_callbacks();
